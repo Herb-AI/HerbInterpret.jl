96afc8a270603a5e7a6286256eb1ceb68606476a
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2024-02-20 11:37:55 +0100

    Add benchmark comparing interpretted vs. compiled expression

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,16 @@
+using BenchmarkTools
+
+const SUITE = BenchmarkGroup()
+
+SUITE["interpret"] = BenchmarkGroup()
+
+tab = Dict{Symbol, Any}(
+    :%      => rem,
+    :(==)   => ==,
+    :string => string,
+    :Int    => Int64,
+    :String => String,
+)
+
+SUITE["interpret"]["compiled"] = @benchmarkable example = (input1) -> if input1 % 5 == 0 && input1 % 3 == 0 return "FizzBuzz" else string(input1) end
+SUITE["interpret"]["compiled"] = @benchmarkable interpret(tab, :(if input1 % 5 == 0 && input1 % 3 == 0 return "FizzBuzz" else string(input1) end))
