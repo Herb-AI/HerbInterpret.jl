64b2c40823112cfe402989224a2e811bb278a5a4
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2026-03-26 15:01:44 +0100

    benchmarks: update the lone benchmark to generated interpret

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -2,13 +2,8 @@ using BenchmarkTools
 using Random: seed!
 using HerbGrammar: @csgrammar, grammar2symboltable, rulenode2expr
 using HerbCore: RuleNode
-# currently this is defined in Search, but should ideally have a definition in Core
-# because having to add Search here in the benchmark environment creates annoying
-# circular dependencies. For now, store the expressions tested in `exprs.jl`
-# using HerbSearch: rand
-using HerbInterpret: interpret
-
-include("exprs.jl")
+using HerbSearch: BFSIterator
+using HerbInterpret: make_interpreter
 
 function create_interpret_benchmark()
     suite = BenchmarkGroup()
@@ -20,12 +15,8 @@ function create_interpret_benchmark()
         Var = |(0:5)
     end
 
-    # once we move random RuleNode sampling to Core
-    # exprs = [rulenode2expr(rand(RuleNode, g), g) for _ in 1:1000]
-
-    st = grammar2symboltable(g)
-
-    suite["Random Expressions"] = @benchmarkable interpret.(($st,), $EXPRS)
+    interpret = make_interpreter(g; cache_module=@__MODULE__, target_module=@__MODULE__)
+    suite["Random Expressions"] = @benchmarkable interpret.($EXPRS)
 
     return suite
 end
