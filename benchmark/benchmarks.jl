e34852616cb3d7c41c7dbf91a89909e7373adf52
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2026-03-26 18:08:42 +0100

    [WIP] switched benchmark module to the interpret-calling module, generated now much faster--maybe too fast??

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,13 +1,17 @@
 using BenchmarkTools
-using Random: seed!
-using HerbGrammar: @csgrammar, grammar2symboltable, rulenode2expr
-using HerbCore: RuleNode
+using HerbGrammar: @csgrammar, expr2rulenode, grammar2symboltable, rulenode2expr
+using HerbCore: HerbCore, RuleNode
 using HerbSearch: BFSIterator
-using HerbInterpret: make_interpreter
+using HerbConstraints: freeze_state
+using HerbInterpret: make_interpreter, execute_on_input
+using HerbBenchmarks: PBE_BV_Track_2018 as BV
+using HerbBenchmarks: PBE_SLIA_Track_2019 as SLIA
+using HerbBenchmarks: get_problem_grammar_pair
+using RuntimeGeneratedFunctions
+RuntimeGeneratedFunctions.init(@__MODULE__)
 
 function create_interpret_benchmark()
     suite = BenchmarkGroup()
-    seed!(42) # keep random expressions constant
     g = @csgrammar begin
         Var = Var + Var
         Var = Var * Var
@@ -15,8 +19,37 @@ function create_interpret_benchmark()
         Var = |(0:5)
     end
 
-    interpret = make_interpreter(g; cache_module=@__MODULE__, target_module=@__MODULE__)
-    suite["Random Expressions"] = @benchmarkable interpret.($EXPRS)
+    # rns = BFSIterator(g, :Var; max_depth=3)
+    #
+    # interpret = make_interpreter(g; cache_module=@__MODULE__, target_module=@__MODULE__)
+    # suite["Random Expressions"] = @benchmarkable interpret.($rns)
+
+    return suite
+end
+
+function create_herbbench_benchmark(benchmark_module, problem_name)
+    suite = BenchmarkGroup()
+    pgp = get_problem_grammar_pair(benchmark_module, problem_name)
+    spec = pgp.problem.spec
+    g = pgp.grammar
+    st = grammar2symboltable(g, benchmark_module)
+    @info "Collecting expressions to benchmark" mod = benchmark_module prob = problem_name
+    it = BFSIterator(g, :Start; max_depth=4, max_size=8)
+    interpret = make_interpreter(g; cache_module=@__MODULE__, target_module=benchmark_module)
+
+    rns = [freeze_state(p) for p in it]
+    @info "Expressions collected" length(rns) type = typeof(rns) examples = rns
+
+    suite["$(length(rns)) expressions"]["generated"] = @benchmarkable try
+        $interpret.($rns, ($spec,))
+    catch
+    end
+
+    suite["$(length(rns)) expressions"]["rulenode2expr"] = @benchmarkable try
+        exprs = rulenode2expr.($rns, ($g,))
+        execute_on_input.(exprs, ($spec,))
+    catch
+    end
 
     return suite
 end
@@ -24,6 +57,8 @@ end
 function create_benchmarks()
     suite = BenchmarkGroup()
     suite["interpret"] = create_interpret_benchmark()
+    suite["HerbBenchmark grammars"]["BV"] = create_herbbench_benchmark(BV, "PRE_100_10")
+    suite["HerbBenchmark grammars"]["SLIA"] = create_herbbench_benchmark(SLIA, "11604909")
     return suite
 end
 
